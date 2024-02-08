c8a96aee4a6161e7c0e4f6ce05bbcb5f1a89d431
Tiw <anthonybatanes@gmail.com>
2024-02-08 17:59:49 +0800

    Update nav and portfolio section

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,41 +1,83 @@
-import React from "react";
+import React, { useState } from 'react';
 import AJLogo from '../assets/AJLogo.svg'
 import { ReactSVG } from 'react-svg';
 
 function Navigation() {
+
+    const [hamburgerOpen, setHamburgerOpen] = useState(true);
+
+    const toggleHamburger = () => {
+        setHamburgerOpen(!hamburgerOpen);
+        console.log({ hamburgerOpen });
+        if (hamburgerOpen) {
+            document.querySelector('.nav-drawer').style.height = '100%';
+        } else {
+            document.querySelector('.nav-drawer').style.height = '0';
+        }
+    }
+
+
     return (
-        <nav className="bg-white dark:bg-black md:fixed w-full z-20 top-0 start-0 shadow shadow-zinc-100 dark:shadow-zinc-800">
-            <div className="max-w-full flex flex-wrap items-center justify-between mx-auto px-20 p-4">
-                <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <div className="w-8 h-auto">
-                        <ReactSVG src={AJLogo} alt="AJLogo" evalScripts="never" className="transition-colors duration-0 text-black dark:text-white" />
+        <>
+            <nav className="bg-white dark:bg-black fixed w-full z-30 top-0 start-0 shadow shadow-zinc-100 dark:shadow-zinc-800">
+                <div className="max-w-full flex flex-wrap items-center justify-between mx-auto px-5 lg:px-20 p-4">
+                    <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                        <div className="w-8 h-auto">
+                            <ReactSVG src={AJLogo} alt="AJLogo" evalScripts="never" className="transition-colors duration-0 text-black dark:text-white" />
+                        </div>
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">AJ Batanes</span>
+                    </a>
+                    <button onClick={toggleHamburger} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                        <span className="sr-only">Open main menu</span>
+                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+                        </svg>
+                    </button>
+                    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+                            <li>
+                                <a href="#intro" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#portfolio" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                                    Portfolio
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#about" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                                    About
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#contact" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
                     </div>
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">AJ Batanes</span>
-                </a>
-                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                    <span className="sr-only">Open main menu</span>
-                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-                    </svg>
-                </button>
-                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
-                        <li>
+                </div>
+            </nav>
+            <div className="md:hidden nav-drawer fixed overflow-hidden top-0 right-0 h-0 w-full z-20 pt-16">
+                <div className="w-full bg-white dark:bg-black h-full">
+                    <ul className="font-medium flex flex-col p-4">
+                        <li className="">
                             <a href="#intro" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                                 Home
                             </a>
                         </li>
-                        <li>
+                        <li className="">
                             <a href="#portfolio" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                                 Portfolio
                             </a>
                         </li>
-                        <li>
+                        <li className="">
                             <a href="#about" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                                 About
                             </a>
                         </li>
-                        <li>
+                        <li className="">
                             <a href="#contact" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                                 Contact
                             </a>
@@ -43,7 +85,7 @@ function Navigation() {
                     </ul>
                 </div>
             </div>
-        </nav>
+        </>
 
     );
 }
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -12,12 +12,12 @@ const Portfolio = () => {
         <>
             <RichText heading="Portfolio" subheading="Notable Projects" />
             <div className="grid grid-cols-1 md:grid-cols-2 gap-16 max-w-screen-xl mx-auto my-20">
-                <ImageHighlight image={Penshoppe} heading="Penshoppe" text="Design, Website Development, Custom Software Integrations, BAUs" />
-                <ImageHighlight image={OXGN} heading="OXGN" text="Full Website Revamp, Design, Website Development, Custom Software Integrations, BAUs" />
-                <ImageHighlight image={Regatta} heading="Regatta" text="Design, Website Development, Custom Software Integrations, BAUs" />
-                <ImageHighlight image={ForMe} heading="ForMe" text="Full Website Revamp, Design, Website Development, Custom Software Integrations, BAUs" />
-                <ImageHighlight image={Memo} heading="Memo" text="Shop By Collection Revamp, Design, Website Development, Custom Software Integrations, BAUs" />
-                <ImageHighlight image={Bocu} heading="Bocu" text="Design, Website Development, Custom Software Integrations, BAUs" />
+                <ImageHighlight image={Penshoppe} heading="Penshoppe" text="Design, Website Development, Custom Software Integrations" />
+                <ImageHighlight image={OXGN} heading="OXGN" text="Full Website Revamp, Design, Website Development, Custom Software Integrations" />
+                <ImageHighlight image={Regatta} heading="Regatta" text="Design, Website Development, Custom Software Integrations" />
+                <ImageHighlight image={ForMe} heading="ForMe" text="Full Website Revamp, Design, Website Development, Custom Software Integrations" />
+                <ImageHighlight image={Memo} heading="Memo" text="Shop By Collection Revamp, Design, Website Development, Custom Software Integrations" />
+                <ImageHighlight image={Bocu} heading="Bocu" text="Design, Website Development, Custom Software Integrations" />
             </div>
         </>
     );

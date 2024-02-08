83937e2dc7308272e326d317fc2422e3ea45c03a
Tiw <anthonybatanes@gmail.com>
2024-02-08 15:22:27 +0800

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { RichText } from './sections/custom-snippets';
 import ContactEmailForm from './sections/contact-email-form';
-import Socials from './sections/Socials';
+import Socials from './sections/socials';
 
 const Contact = () => {
     return (

78a87244779abdeacfda75af209037d02b88d0cb
ecommchannel <anthonybatanes@gmail.com>
2024-02-11 03:02:52 +0800

    update footer css

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="fixed bottom-0 w-full h-52 md:h-32 bg-black dark:bg-white -z-10">
             <div className="max-w-full h-full flex align-middle flex-wrap items-center justify-center mx-auto px-20 p-4">
-                <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <a href="#intro" className="flex flex-col md:flex-row justify-center items-center space-x-3 rtl:space-x-reverse">
                     <div className="w-12 h-auto m-2">
                         <img src={AJLogo} alt={AJLogo} />
                     </div>

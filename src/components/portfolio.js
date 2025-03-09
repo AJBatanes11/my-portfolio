7a2def5eb5e028fb8edcdad8bf711fa471e5dec1
AJBatanes11 <anthonybatanes@gmail.com>
2025-03-09 14:46:30 +0800

    Add carousel and update portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -15,6 +15,13 @@ import TheCutBuddy from '../assets/TheCutBuddy.jpg';
 import TheHoopGang from '../assets/TheHoopGang.jpg';
 import HenkanCollagen from '../assets/HenkanCollagen.jpg';
 import HillsideGarden from '../assets/HillsideGarden.jpg';
+import ChakoLab from '../assets/ChakoLab.png';
+import { Grid, Navigation, Pagination, Autoplay, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import "swiper/css/grid";
 
 const Portfolio = ({ hoverStatusPortfolio }) => {
 
@@ -24,7 +31,7 @@ const Portfolio = ({ hoverStatusPortfolio }) => {
 
     return (
         <>
-            <div className="max-w-screen-xl mx-auto">
+            <div className="max-w-screen-xl mx-auto w-full">
                 <RichText
                     heading="Recent Work"
                     subheading=""
@@ -73,70 +80,120 @@ const Portfolio = ({ hoverStatusPortfolio }) => {
                         heading="Bocu"
                         text="Full Website Revamp, Design, Website Development, Custom Software Integrations"
                     />
-                    <ImageHighlight
-                        hoverstatus={handleHoverStatus}
-                        image={TheHoopGang}
-                        link="https://thehoopgang.com/"
-                        heading="The Hoop Gang"
-                        text="Design, Website Revamp, Website Development, Custom Sections"
-                    />
-                    <ImageHighlight
-                        hoverstatus={handleHoverStatus}
-                        image={SoulAesthethix}
-                        link="https://soulaesthetix.com.au/"
-                        heading="SoulAesthetix"
-                        text="Design, Website Revamp, Website Development, Custom Sections"
-                    />
-                    <ImageHighlight
-                        hoverstatus={handleHoverStatus}
-                        image={TheCutBuddy}
-                        link="https://thecutbuddy.com/"
-                        heading="The Cut Buddy"
-                        text="Design, Website Revamp, Website Development, Custom Sections"
-                    />
-                    <ImageHighlight
-                        hoverstatus={handleHoverStatus}
-                        image={ArianaSpices}
-                        link="https://arianaspices.com/"
-                        heading="Ariana Spices"
-                        text="Design, Website Revamp, Website Development, Custom Sections"
-                    />
-                    <ImageHighlight
-                        hoverstatus={handleHoverStatus}
-                        image={HillsideGarden}
-                        link="https://hillsidegarden.com.ph/"
-                        heading="Hillside Garden"
-                        text="Design, Website Revamp, Website Development, Custom Sections"
-                    />
-                    <ImageHighlight
-                        hoverstatus={handleHoverStatus}
-                        image={HenkanCollagen}
-                        link="https://henkancollagen.myshopify.com/"
-                        heading="Henkan Collagen"
-                        text="Design, Website Revamp, Website Development, Custom Sections"
-                    />
-                    <ImageHighlight
-                        hoverstatus={handleHoverStatus}
-                        image={LaunchtoProfitMedia}
-                        link="https://www.launchtoprofitmedia.com/"
-                        heading="Launch to Profit Media"
-                        text="Design, Website Revamp, Website Development, Custom Sections"
-                    />
-                    <ImageHighlight
-                        hoverstatus={handleHoverStatus}
-                        image={ModandNoble}
-                        link="https://www.modandnoble.com/"
-                        heading="Mod & Noble"
-                        text="Design, End-to-end website creation, Website Development, Process Improvement & Consulting Services"
-                    />
-                    <ImageHighlight
-                        hoverstatus={handleHoverStatus}
-                        image={Tiwflix}
-                        link="https://ajbatanes11.github.io/tiwflix/"
-                        heading="Tiwflix"
-                        text="Personal Website, Design, End-to-end Development, API Integration"
-                    />
                 </div>
+                <Spacer size="2.5rem" />
+                <h2 className="text-5xl xl:text-7xl font-normal text-black dark:text-white mx-auto text-center">More Work</h2>
+                <Spacer size="2.5rem" />
+                <Swiper
+                    modules={[Grid, Navigation, Pagination, Autoplay, A11y]}
+                    spaceBetween={50}
+                    pagination={{ clickable: true }}
+                    breakpoints={{
+                        0: {
+                            slidesPerView: 1.1,
+                            grid: { rows: 1 }
+                        },
+                        768: {
+                            slidesPerView: 2.2, // 2 columns
+                            grid: { rows: 2, fill: "row" }
+                        },
+                        1024: {
+                            slidesPerView: 3.2, // 2 columns
+                            grid: { rows: 2, fill: "row" }
+                        }
+                    }}
+                >
+                    <SwiperSlide>
+                        <ImageHighlight
+                            hoverstatus={handleHoverStatus}
+                            image={ChakoLab}
+                            link="https://chakolabph.com/"
+                            heading="Chako Lab"
+                            text="Design, Website Revamp, Website Development, Custom Sections"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <ImageHighlight
+                            hoverstatus={handleHoverStatus}
+                            image={TheCutBuddy}
+                            link="https://thecutbuddy.com/"
+                            heading="The Cut Buddy"
+                            text="Design, Website Revamp, Website Development, Custom Sections"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <ImageHighlight
+                            hoverstatus={handleHoverStatus}
+                            image={TheHoopGang}
+                            link="https://thehoopgang.com/"
+                            heading="The Hoop Gang"
+                            text="Design, Website Revamp, Website Development, Custom Sections"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <ImageHighlight
+                            hoverstatus={handleHoverStatus}
+                            image={HillsideGarden}
+                            link="https://hillsidegarden.com.ph/"
+                            heading="Hillside Garden"
+                            text="Design, Website Revamp, Website Development, Custom Sections"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <ImageHighlight
+                            hoverstatus={handleHoverStatus}
+                            image={HenkanCollagen}
+                            link="https://henkancollagen.myshopify.com/"
+                            heading="Henkan Collagen"
+                            text="Design, Website Revamp, Website Development, Custom Sections"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <ImageHighlight
+                            hoverstatus={handleHoverStatus}
+                            image={SoulAesthethix}
+                            link="https://soulaesthetix.com.au/"
+                            heading="Soul Aesthetix"
+                            text="Design, Website Revamp, Website Development, Custom Sections"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <ImageHighlight
+                            hoverstatus={handleHoverStatus}
+                            image={ArianaSpices}
+                            link="https://arianaspices.com/"
+                            heading="Ariana Spices"
+                            text="Design, Website Revamp, Website Development, Custom Sections"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <ImageHighlight
+                            hoverstatus={handleHoverStatus}
+                            image={LaunchtoProfitMedia}
+                            link="https://www.launchtoprofitmedia.com/"
+                            heading="Launch to Profit Media"
+                            text="Design, Website Revamp, Website Development, Custom Sections"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <ImageHighlight
+                            hoverstatus={handleHoverStatus}
+                            image={ModandNoble}
+                            link="https://www.modandnoble.com/"
+                            heading="Mod & Noble"
+                            text="Design, End-to-end website creation, Website Development, Process Improvement & Consulting Services"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <ImageHighlight
+                            hoverstatus={handleHoverStatus}
+                            image={Tiwflix}
+                            link="https://ajbatanes11.github.io/tiwflix/"
+                            heading="Tiwflix"
+                            text="Personal Website, Design, End-to-end Development, API Integration"
+                        />
+                    </SwiperSlide>
+                </Swiper>
             </div>
         </>
     );
